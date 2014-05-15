4baf0ce60532f0c80b28215624e0c256f66eaf8b
peinan <peinan7@gmail.com>
2014-05-15 15:36:26 +0900

    word segment

--- ch04_word_segment/unigram.py
+++ ch04_word_segment/unigram.py
@@ -13,16 +13,16 @@ def train_unigram(train_file):
 	
 	for line in open(train_file, "r"):
 		words = line.strip().split(" ")
-		words.append("</s>")
+		# words.append("</s>")
 		for word in words:
 			count_of[word] += 1
 			total_count    += 1
 
-	model_file = open("unigram.model", "w")
-	for k, v in count_of.items():
-		prob = float(count_of[k]) / total_count
-		# print k, prob
-		model_file.write("%s\t%f\n" % (k, prob))
+	model_file = "unigram.model"
+	with open(model_file, "w") as model_file:
+		for k, v in count_of.items():
+			prob = float(count_of[k]) / total_count
+			model_file.write("%s\t%f\n" % (k, prob))
 
 
 def calc_prob(word, unigram_of):
@@ -42,6 +42,7 @@ def import_model(model_file):
 	for line in open(model_file, "r"):
 		line = line.strip()
 		word, prob = line.split("\t")
+		word = word.decode("utf-8")
 		prob_of[word] = float(prob)
 	return prob_of
 
@@ -76,4 +77,4 @@ def test_unigram(model_file, test_file):
 
 if __name__ == "__main__":
 	train_unigram(sys.argv[1])
-	test_unigram("unigram.model", sys.argv[2])
+	# test_unigram("unigram.model", sys.argv[2])
